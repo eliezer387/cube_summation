0b5058fee117f50166ddbcbe9b7aef9d9abc9fd5
Your Name <you@example.com>
2016-03-13 13:32:19 -0430

    Mejoras en la vista

--- resources/views/cube.php
+++ resources/views/cube.php
@@ -1,6 +1,18 @@
-
-<?php ?>
-<form name="entrada" action="submit" method="post">
-    <textarea name="ope" value=""></textarea>
-    <input type="submit" value="enviar">
-</form>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    </head>
+    <?php ?>
+    <content>
+        <form class="form-inline" name="entrada" action="submit" method="post">
+            <div class="form-group">
+                <label>Inserte Las Operaciones a Realizar </label>
+                <textarea class="form-control" rows="8" name="ope" value=""></textarea>
+            </div>
+            <div class="form-group">
+                <input class="btn btn-primary" type="submit" value="enviar">
+            </div>
+        </form>
+    </content>
+</html>    
