f83d3601557da6d62b6b9034a49a09facd99df4d
Your Name <you@example.com>
2016-03-13 19:47:19 -0430

    creacion de vista para mostrar el resultado de las operaciones

---
+++ resources/views/show_results.php
@@ -0,0 +1,16 @@
+<?php
+//var_dump($results);
+?>
+
+<html>
+    <head>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    </head>
+    
+    <section>
+        <pre class="pre-scrollable">
+            <?php echo ($results) ?>
+        </pre> 
+    </section>
+
+</html>
