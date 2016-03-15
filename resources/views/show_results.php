c1ced6785d73e5bf4fa4146af27d82161cb8aa97
Eliezer Pe#a <pe.eliezer@gmail.com>
2016-03-14 23:56:52 -0430

    retoques en interfaz

--- resources/views/show_results.php
+++ resources/views/show_results.php
@@ -1,15 +1,3 @@
-<!--<html>
-    <head>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    </head>
-    
-    <section>
-        
-    </section>
-
-</html>-->
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -30,17 +18,8 @@
         <style>
             body {
                 padding-top: 70px;
-                /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
             }
         </style>
-
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
-
     </head>
 
     <body>
